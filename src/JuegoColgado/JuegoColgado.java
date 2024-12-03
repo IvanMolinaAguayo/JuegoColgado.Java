480170d4b0ab46b39b310c18f30a2499c7173ab0
IvanMolinaAguayo <ivimolinaaguayo@gmail.com>
2024-12-03 16:50:44 +0100

    Primeras variables
    En este primer commit he declarado las primeras variables

---
+++ src/JuegoColgado/JuegoColgado.java
@@ -0,0 +1,16 @@
+package JuegoColgado;
+import java.util.Scanner;
+public class JuegoColgado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        int jugadors;
+        int maxIntents = 6;
+        int rondes;
+        String palabraSecreta;
+        String palabraOculta;
+
+	}
+
+}
