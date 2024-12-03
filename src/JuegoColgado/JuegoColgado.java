2898d35d024e47d0cf54d81a2d0166a5bc760118
IvanMolinaAguayo <ivimolinaaguayo@gmail.com>
2024-12-03 16:53:38 +0100

    Jugadores totales
    Pedimos el número total de jugadores

--- src/JuegoColgado/JuegoColgado.java
+++ src/JuegoColgado/JuegoColgado.java
@@ -5,11 +5,22 @@ public class JuegoColgado {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner scr = new Scanner(System.in);
+		
         int jugadors;
         int maxIntents = 6;
         int rondes;
         String palabraSecreta;
         String palabraOculta;
+
+        
+        //Pedimos el número de jugadores totales
+        System.out.println("Juego del colgado");
+        do {
+            System.out.println("¿Cuántos jugadores van a participar? (Mínimo 2 jugadores): ");
+            jugadors = scr.nextInt();
+            scr.nextLine(); 
+        } while (jugadors < 2);
 
 	}
 
