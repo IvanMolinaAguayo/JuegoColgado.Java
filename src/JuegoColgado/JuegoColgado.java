73a9b23f8bc5932b7917317bc23f3acf823ba8b4
IvanMolinaAguayo <ivimolinaaguayo@gmail.com>
2024-12-09 17:19:32 +0100

    Random y traducción
    Agregamos el random y traducimos ciertas palabras

--- src/JuegoColgado/JuegoColgado.java
+++ src/JuegoColgado/JuegoColgado.java
@@ -1,136 +1,140 @@
 package JuegoColgado;
+import java.util.Random;
 import java.util.Scanner;
+
 public class JuegoColgado {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
 
-		Scanner scr = new Scanner(System.in);
-		
+        Scanner scr = new Scanner(System.in);
+
         int jugadors;
         int maxIntents = 6;
         int rondes;
-        String paraulaSecreta;
-        String paraulaOculta;
-
+        String palabraSecreta;
+        String palabraOculta;
         
-        //Pedimos el número de jugadores totales
+        // Listado de palabras (ciudades)
+        String[] listaDePalabras = {
+            "Barcelona", "Madrid", "Bilbao", "Valencia", "Sevilla", "Vigo", "Tarragona", "Málaga", "Murcia", "Lérida"
+        };
+        
+        //  Cantidad de jugadores
         System.out.println("Juego del colgado");
         do {
             System.out.println("¿Cuántos jugadores van a participar? (Mínimo 2 jugadores): ");
             jugadors = scr.nextInt();
-            scr.nextLine(); 
+            scr.nextLine();
         } while (jugadors < 2);
-        
-        //Pedimos el número de rondas que se van a jugar
-        System.out.println("¿Cuántas rondas van a jugar?");
+
+        // Cantidad de rondas
+        System.out.println("¿Cuántas rondas se van a jugar?");
         rondes = scr.nextInt();
-        scr.nextLine();
-        
-        //Nombres de los jugadores
-        String[]nomJugadors = new String[jugadors];
-        for (int i = 0;i <jugadors;i++) {
-        	System.out.println("Ingresa el nombre del jugador"+(i+1)+":");
-        	nomJugadors[i]=scr.nextLine();
+        scr.nextLine(); 
+
+        // Nombres de los jugadores
+        String[] nombresJugadores = new String[jugadors];
+        for (int i = 0; i < jugadors; i++) {
+            System.out.println("Ingresa el nombre del jugador " + (i + 1) + ": ");
+            nombresJugadores[i] = scr.nextLine();
         }
-        
-        //Recuento de las victorias de cada jugador
-        int[] victories = new int[jugadors];
-        
-        //Rondes
-        for(int ronda = 1; ronda <= rondes; ronda++) {
-        	System.out.println("\nRonda" + ronda +" comenzando...");
-        	
-        	   // Elegir la palabra secreta
-            System.out.println(nomJugadors[0] + ", ingresa la palabra secreta: ");
-            paraulaSecreta = scr.nextLine().toLowerCase();
-            while (paraulaSecreta.isEmpty()) {
-                System.out.println("La palabra secreta no puede estar en blanco.");
-                paraulaSecreta = scr.nextLine().toLowerCase();
-            }
-            
-            // Ocultamos  la palabra secreta
-            paraulaOculta = paraulaSecreta.replaceAll(".", "_");
 
-            // Inicializamos los intentos
-            int[] intentsRestants = new int[jugadors];
+        // Victorias de cada jugador
+        int[] victorias = new int[jugadors];
+
+        // Ciclo por rondas
+        for (int ronda = 1; ronda <= rondes; ronda++) {
+            System.out.println("\nRonda " + ronda + " comenzando...");
+            
+            // Elección de la palabra aleatoria
+            Random rand = new Random();
+            palabraSecreta = listaDePalabras[rand.nextInt(listaDePalabras.length)];
+            System.out.println("La palabra secreta ha sido seleccionada (es aleatoria y está oculta para los jugadores).");
+
+            // Ocultar la palabra secreta
+            palabraOculta = palabraSecreta.replaceAll(".", "_");
+
+            // Inicializar los intentos
+            int[] intentosRestantes = new int[jugadors];
             for (int i = 0; i < jugadors; i++) {
-                intentsRestants[i] = maxIntents;
-
+                intentosRestantes[i] = maxIntents;
             }
-            
-            // Empieza el juego 
-            boolean paraulaAdivinada = false;
-            while (!paraulaAdivinada) {
+
+            // Inicio del juego
+            boolean palabraAdivinada = false;
+            while (!palabraAdivinada) {
                 for (int i = 0; i < jugadors; i++) {
-                    if (intentsRestants[i] <= 0) {
-                        System.out.println(nomJugadors[i] + " se ha quedado sin intentos.");
+                    if (intentosRestantes[i] <= 0) {
+                        System.out.println(nombresJugadores[i] + " se ha quedado sin intentos.");
                         continue;
                     }
-                    
-                    System.out.println("nTurno de " + nomJugadors[i]);
-                    System.out.println("Palabra oculta: " + paraulaOculta);
-                    System.out.println("Intentos restantes: " + intentsRestants[i]);
+
+                    System.out.println("\nTurno de " + nombresJugadores[i]);
+                    System.out.println("Palabra oculta: " + palabraOculta);
+                    System.out.println("Intentos restantes: " + intentosRestantes[i]);
                     System.out.print("Ingresa una letra: ");
-                    String lletra = scr.nextLine().toLowerCase();
-                    
-                    // Verificamos que la letra es válida
-                    if (lletra.length() != 1 || !Character.isLetter(lletra.charAt(0))) {
+                    String letra = scr.nextLine().toLowerCase();
+
+                    // Verificar si la letra es válida
+                    if (letra.length() != 1 || !Character.isLetter(letra.charAt(0))) {
                         System.out.println("Por favor, ingresa solo una letra.");
-                        i--;
-                        continue;   
-                        }
-                    // Comprovamos que la letra elegida está en la palabra oculta
-                    if (paraulaSecreta.contains(lletra)) {
+                        i--; // Repetir el turno del jugador
+                        continue;
+                    }
+
+                    // Comprobar si la letra está en la palabra secreta
+                    if (palabraSecreta.contains(letra)) {
                         System.out.println("¡Correcto!");
-                        // Si la letra se encuentra en la palabra, actualizamos los espacios vacios donde se encuntra la palabra
-                        StringBuilder novaparaulaOculta = new StringBuilder(paraulaOculta);
-                        for (int j = 0; j < paraulaSecreta.length(); j++) {
-                            if (paraulaSecreta.charAt(j) == lletra.charAt(0)) {
-                                novaparaulaOculta.setCharAt(j, lletra.charAt(0));
+                        // Actualizar la palabra oculta
+                        StringBuilder nuevaPalabraOculta = new StringBuilder(palabraOculta);
+                        for (int j = 0; j < palabraSecreta.length(); j++) {
+                            if (palabraSecreta.charAt(j) == letra.charAt(0)) {
+                                nuevaPalabraOculta.setCharAt(j, letra.charAt(0));
                             }
                         }
-                        paraulaOculta = novaparaulaOculta.toString();
+                        palabraOculta = nuevaPalabraOculta.toString();
                     } else {
                         System.out.println("¡Incorrecto!");
-                        intentsRestants[i]--;
+                        intentosRestantes[i]--;
                     }
-                    // Verificamos si la palabra ha sido adivinada
-                    if (paraulaOculta.equals(paraulaSecreta)) {
-                        paraulaAdivinada = true;
-                        victories[i]++;
-                        System.out.println(nomJugadors[i] + " ha adivinado la palabra: " + paraulaSecreta);
-                        break;
 
+                    // Verificar si la palabra ha sido adivinada
+                    if (palabraOculta.equals(palabraSecreta)) {
+                        palabraAdivinada = true;
+                        victorias[i]++;
+                        System.out.println(nombresJugadores[i] + " ha adivinado la palabra: " + palabraSecreta);
+                        break;
                     }
-                    // Verificamos si algún jugador se ha quedado sin intentos 
-                    if (intentsRestants[i] <= 0) {
-                        System.out.println(nomJugadors[i] + " se ha quedado sin intentos.");
-                    }
-                    // Mostramos la puntuación final de los jugadores
-                    System.out.println("\nPuntajes finales:");
-                    int maxPuntatges = 0;
-                    for (int j = 0; j < victories.length; i++) {
-                        System.out.println(nomJugadors[i] + ": " + victories[i]);
-                        if (victories[i] > maxPuntatges) {
-                            maxPuntatges = victories[i];
-                        }
-                    }
-                    // Determinamos  y mostramos el ganador
-                    System.out.println("\n¡Ganadores!");
-                    for (int j = 0; j < victories.length; i++) {
-                        if (victories[i] == maxPuntatges) {
-                            System.out.println(nomJugadors[i]);
-                        }
+
+                    // Verificar si se han quedado sin intentos
+                    if (intentosRestantes[i] <= 0) {
+                        System.out.println(nombresJugadores[i] + " se ha quedado sin intentos.");
                     }
                 }
-               
-
             }
-                  
         }
-        
 
-	}
+        // Mostrar los puntajes finales
+        System.out.println("\nPuntajes finales:");
+        int maxPuntajes = 0;
+        for (int i = 0; i < victorias.length; i++) {
+            System.out.println(nombresJugadores[i] + ": " + victorias[i]);
+            if (victorias[i] > maxPuntajes) {
+                maxPuntajes = victorias[i];
+            }
+        }
 
+        // Determinar y mostrar el ganador
+        System.out.println("\n¡Ganadores!");
+        for (int i = 0; i < victorias.length; i++) {
+            if (victorias[i] == maxPuntajes) {
+                System.out.println(nombresJugadores[i]);
+            }
+        }
+
+        scr.close();
+    }
 }
+
+
+               
